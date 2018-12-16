5e26320180244cdc116de693e025823e97bd11e8
Andrew Pungprakearti <andrew.pungprakearti@gmail.com>
2018-12-15 16:32:36 -0800

    Testing webpack stuff.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -8,56 +8,62 @@ export default class NavBar extends Component {
     let navbar
 
     //for mobile
-    console.log(window.innerWidth)
-    if (window.innerWidth <= 415) {
-      navbar = (
-        <div className="NavBar-cont">
-          <Link to="/">
-            <FaHome className="NavBar-icon-home" />
-          </Link>
-          <Link to="/resume">
-            <FaFileAlt className="NavBar-icon-resume" />
-          </Link>
-          <a href="https://github.com/pungprakearti">
-            <FaGithubSquare className="NavBar-icon-github" />
-          </a>
-          <Link to="/contact">
-            <FaEnvelope className="NavBar-icon-contact" />
-          </Link>
-        </div>
-      )
-    } else {
-      //
-      //desktop
-      navbar = (
-        <div className="NavBar-cont">
-          <div className="NavBar-left">
+    try {
+      if (window.innerWidth <= 415) {
+        navbar = (
+          <div className="NavBar-cont">
+            <Link to="/">
+              <FaHome className="NavBar-icon-home" />
+            </Link>
+            <Link to="/resume">
+              <FaFileAlt className="NavBar-icon-resume" />
+            </Link>
+            <a href="https://github.com/pungprakearti">
+              <FaGithubSquare className="NavBar-icon-github" />
+            </a>
+            <Link to="/contact">
+              <FaEnvelope className="NavBar-icon-contact" />
+            </Link>
+          </div>
+        )
+      } else {
+        //
+        //desktop
+        navbar = (
+          <div className="NavBar-cont">
+            <div className="NavBar-left">
+              <div
+                className="NavBar-name"
+                style={{ color: this.props.colorName }}
+              >
+                <Link to="/">Andrew Pungprakearti</Link>
+              </div>
+              <div
+                className="NavBar-title"
+                style={{ color: this.props.colorTitle }}
+              >
+                Full Stack Developer
+              </div>
+            </div>
             <div
-              className="NavBar-name"
+              className="NavBar-right"
               style={{ color: this.props.colorName }}
             >
-              <Link to="/">Andrew Pungprakearti</Link>
-            </div>
-            <div
-              className="NavBar-title"
-              style={{ color: this.props.colorTitle }}
-            >
-              Full Stack Developer
-            </div>
-          </div>
-          <div className="NavBar-right" style={{ color: this.props.colorName }}>
-            <div className="NavBar-right-link">
-              <Link to="/resume">Resume</Link>
-            </div>
-            <div className="NavBar-right-link">
-              <a href="https://github.com/pungprakearti">Github</a>
-            </div>
-            <div className="NavBar-right-link">
-              <Link to="/contact">Contact</Link>
+              <div className="NavBar-right-link">
+                <Link to="/resume">Resume</Link>
+              </div>
+              <div className="NavBar-right-link">
+                <a href="https://github.com/pungprakearti">Github</a>
+              </div>
+              <div className="NavBar-right-link">
+                <Link to="/contact">Contact</Link>
+              </div>
             </div>
           </div>
-        </div>
-      )
+        )
+      }
+    } catch (err) {
+      console.log(err)
     }
 
     return navbar

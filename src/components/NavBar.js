cf80297483f0ad8fc95a5e6e964d424243f92a4e
Andrew Pungprakearti <andrew.pungprakearti@gmail.com>
2018-12-15 16:37:16 -0800

    Trying to fix an issue with window and webpack.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -63,7 +63,35 @@ export default class NavBar extends Component {
         )
       }
     } catch (err) {
-      console.log(err)
+      navbar = (
+        <div className="NavBar-cont">
+          <div className="NavBar-left">
+            <div
+              className="NavBar-name"
+              style={{ color: this.props.colorName }}
+            >
+              <Link to="/">Andrew Pungprakearti</Link>
+            </div>
+            <div
+              className="NavBar-title"
+              style={{ color: this.props.colorTitle }}
+            >
+              Full Stack Developer
+            </div>
+          </div>
+          <div className="NavBar-right" style={{ color: this.props.colorName }}>
+            <div className="NavBar-right-link">
+              <Link to="/resume">Resume</Link>
+            </div>
+            <div className="NavBar-right-link">
+              <a href="https://github.com/pungprakearti">Github</a>
+            </div>
+            <div className="NavBar-right-link">
+              <Link to="/contact">Contact</Link>
+            </div>
+          </div>
+        </div>
+      )
     }
 
     return navbar

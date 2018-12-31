0955050f514baed5b16fa8dbb44624b3031d316d
Andrew Pungprakearti <andrew.pungprakearti@gmail.com>
2018-12-30 16:53:55 -0800

    Changed some fonts and layout of about me page. Still need to clean up and add more functionality to other pages.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -10,29 +10,34 @@ export default class NavBar extends Component {
     navbar = (
       <React.Fragment>
         <div className="NavBar-cont NavBar-desktop">
-          <div className="NavBar-left">
+          <div className="NavBar-inner-cont">
+            <div className="NavBar-left">
+              <div
+                className="NavBar-name"
+                style={{ color: this.props.colorName }}
+              >
+                <Link to="/">Andrew Pungprakearti</Link>
+              </div>
+              <div
+                className="NavBar-title"
+                style={{ color: this.props.colorTitle }}
+              >
+                Full Stack Developer
+              </div>
+            </div>
             <div
-              className="NavBar-name"
+              className="NavBar-right"
               style={{ color: this.props.colorName }}
             >
-              <Link to="/">Andrew Pungprakearti</Link>
-            </div>
-            <div
-              className="NavBar-title"
-              style={{ color: this.props.colorTitle }}
-            >
-              Full Stack Developer
-            </div>
-          </div>
-          <div className="NavBar-right" style={{ color: this.props.colorName }}>
-            <div className="NavBar-right-link">
-              <Link to="/resume">Resume</Link>
-            </div>
-            <div className="NavBar-right-link">
-              <a href="https://github.com/pungprakearti">Github</a>
-            </div>
-            <div className="NavBar-right-link">
-              <Link to="/contact">Contact</Link>
+              <div className="NavBar-right-link">
+                <Link to="/resume">Resume</Link>
+              </div>
+              <div className="NavBar-right-link">
+                <a href="https://github.com/pungprakearti">Github</a>
+              </div>
+              <div className="NavBar-right-link">
+                <Link to="/contact">Contact</Link>
+              </div>
             </div>
           </div>
         </div>
--- src/pages/aboutme.js
+++ src/pages/aboutme.js
@@ -3,6 +3,7 @@ import { Link } from 'gatsby'
 import { graphql } from 'gatsby'
 import Layout from '../components/layout'
 import BackBtn from '../components/BackBtn'
+import './AboutMe.css'
 
 export const query = graphql`
   {
@@ -29,71 +30,92 @@ export default class AboutMePage extends Component {
       <Layout>
         <div className="content-cont">
           <div className="content-center">
-            <br />
-            <img
-              src={this.props.data.imgAboutMe.childImageSharp.fluid.src}
-              alt="Such a handsome fellow. Also, very very humble."
-            />
+            <h1>I’m a full stack developer</h1>
+            <div className="content-2col">
+              {/* <br /> */}
+              <div className="AboutMe-img">
+                <img
+                  src={this.props.data.imgAboutMe.childImageSharp.fluid.src}
+                  alt="Such a handsome fellow. Also, very very humble."
+                />
+              </div>
+              <div className="AboutMe-p">
+                <p>
+                  I’m a full stack developer currently attending Rithm School’s
+                  eighth cohort. From school I’ve learned that I’m a huge fan of
+                  React, CSS, and just working on the front end in general. I
+                  love being able to create apps that look exactly like I
+                  envision them.
+                </p>
+                <p>
+                  Outside of web development, I enjoy making games. I’m half of
+                  Lead Money Games, an independent game development company. On
+                  June 15, 2016 we were funded through Kickstarter and on April
+                  4, 2017 we launched our first game on Steam called "Lil
+                  Tanks." It’s a little shoot ’em up game that was purely
+                  engineered through trial and error and lots of online
+                  tutorials.
+                </p>
+                <div className="content-center">
+                  <Link to="liltanks">Lil Tanks</Link>
+                </div>
+              </div>
+            </div>
           </div>
+
           <p>
-            I'm a full stack developer currently attending Rithm School's eighth
-            cohort. From school I've learned that I'm a huge fan of React, CSS,
-            and just working on the front end in general. I love being able to
-            create apps that look exactly like I envision them.
-          </p>
-          <p>
-            Outside of web development, I enjoy making games. I'm half of Lead
-            Money Games, an independent game development company. On June 15,
-            2016 we were funded through Kickstarter and on April 4, 2017 we
-            launched our first game on Steam called "Lil Tanks." It's a little
-            shoot 'em up game that was purely engineered through trial and error
-            and lots of online tutorials.
-          </p>
-          <div className="content-center">
-            <Link to="liltanks">Lil Tanks</Link>
-          </div>
-          <p>
-            After a few years of game development, I decided it's time for me to
-            throw myself into full fledged web development. I've always loved
-            the intersection of computers and art and I'm excited to see where
+            After a few years of game development, I decided it’s time for me to
+            throw myself into full fledged web development. I’ve always loved
+            the intersection of computers and art and I’m excited to see where
             this takes me.
           </p>
 
-          <p>
-            I also freelance as a digital effects artist having worked on over
-            forty films. This is a vestige of my former life working in the film
-            industry for over 10 years.
-          </p>
           <div className="content-center">
-            <a href="https://www.imdb.com/name/nm3117059">
-              Check out my IMDB page
-            </a>
-          </div>
+            <h2>having worked on over forty films</h2>
+            <div className="content-2col">
+              <div className="AboutMe-p">
+                <p>
+                  I also freelance as a digital effects artist having worked on
+                  over forty films. This is a vestige of my former life working
+                  in the film industry for over 10 years.
+                </p>
+                <div className="content-center">
+                  <a href="https://www.imdb.com/name/nm3117059">
+                    Check out my IMDB page
+                  </a>
+                </div>
 
-          <p>
-            In my free time, you can usually find me on a hockey rink. I play
-            ice and roller hockey all over the Bay Area and Sonoma County. If I
-            can continue to pay league fees, I am happy.
-          </p>
+                <p>
+                  In my free time, you can usually find me on a hockey rink. I
+                  play ice and roller hockey all over the Bay Area and Sonoma
+                  County. If I can continue to pay league fees, I am happy.
+                </p>
 
-          <p>
-            My family life consists of my amazing wife Samantha, and my two
-            adorable children: Theo and Violet.
-            <br />
-          </p>
-          <div className="content-center">
-            <a href="https://imgur.com/gallery/ZMZOU">
-              DITL of an indie game developer
-            </a>
-            <br />
-            <br />
+                <p>
+                  My family life consists of my amazing wife Samantha, and my
+                  two adorable children: Theo and Violet.
+                  <br />
+                </p>
+              </div>
+
+              <div className="AboutMe-img">
+                <img
+                  src={this.props.data.imgFamily.childImageSharp.fluid.src}
+                  alt="Mi familia"
+                />
+                <br />
+                <br />
+                <div className="content-center">
+                  <a href="https://imgur.com/gallery/ZMZOU">
+                    DITL of an indie game developer
+                  </a>
+                </div>
+              </div>
+            </div>
           </div>
-          <div className="content-center">
-            <img
-              src={this.props.data.imgFamily.childImageSharp.fluid.src}
-              alt="Mi familia"
-            />
-          </div>
+          <br />
+          <br />
+          <br />
           <br />
         </div>
         <BackBtn />
